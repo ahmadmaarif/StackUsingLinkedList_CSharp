b8e8a037411e02eea2cc0ecc76f1007a56fde93b
Ahmad Maarif <ahmad.maarif.ft21@mail.umy.ac.id>
2022-12-02 17:23:10 +0700

    Add method Node

--- StackUsingLinkedList_CSharp/Program.cs
+++ StackUsingLinkedList_CSharp/Program.cs
@@ -8,6 +8,12 @@ namespace StackUsingLinkedList_CSharp
 {
    class Node
     {
-
+        public int info;
+        public Node next;
+        public Node(int i, Node n)
+        {
+            info = i;
+            next = n;
+        }
     }
 }
